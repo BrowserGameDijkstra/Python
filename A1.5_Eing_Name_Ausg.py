6e5b5ef9d7e7ba0137532ffa468d42e9bc44b592
HelloRamo <39927941+HelloRamo@users.noreply.github.com>
2022-02-21 20:11:10 +0100

    Delete A1.5_Eing_Name_Ausg.py

--- A1.5_Eing_Name_Ausg.py
+++
@@ -1,5 +0,0 @@
-print("Eingabe des Namens und Ausgabe")
-print("-----------------------------------------------------")
-print()
-Name1 = input("Eingabe ihres Namens: ") # input gibt immer string aus
-print("WillkommenR",Name1)
