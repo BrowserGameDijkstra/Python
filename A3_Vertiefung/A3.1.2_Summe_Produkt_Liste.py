0505fff4e500c88717442ec4d26111d4ba8ad10f
HelloRamo <39927941+HelloRamo@users.noreply.github.com>
2022-02-22 00:00:26 +0100

    Delete A3_Vertiefung directory

--- A3_Vertiefung/A3.1.2_Summe_Produkt_Liste.py
+++
@@ -1,9 +0,0 @@
-# A3.1.2 Summe und das Produkt Liste  Zahlen ermittelt
-werte = [1, 2, 4, 5, 9,12]
-summe = 0.0
-produkt = 1.0
-for i in werte:
-    summe = summe + i
-    produkt = produkt * i
-print("Summe = ",summe)
-print("Produkt =",produkt)
