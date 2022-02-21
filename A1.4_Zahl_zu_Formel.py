828930fc57231df0474557d7c1f5508ca9cddc37
HelloRamo <39927941+HelloRamo@users.noreply.github.com>
2022-02-21 20:11:03 +0100

    Delete A1.4_Zahl_zu_Formel.py

--- A1.4_Zahl_zu_Formel.py
+++
@@ -1,9 +0,0 @@
-from math import *
-print("Eine Zahl, die per Formel sich wandelt")
-print("-----------------------------------------------------")
-print()
-
-x = float(input("Bitte eine Zahl X eingeben eingeben: ")) # definierte Variable, input
-y = (1/2*x**3-1/2*x**2+2*x+5) # ** == potenz
-print()
-print("x=",x,"y=",y)
